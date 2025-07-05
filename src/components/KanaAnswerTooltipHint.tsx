36a15047b6b11d4b42505824634a3904b48c9010
gilcastellar <gilcastellar@gmail.com>
2025-07-05 11:21:54 -0300

    Update KanaAnswerTooltipHint.tsx

--- src/components/KanaAnswerTooltipHint.tsx
+++ src/components/KanaAnswerTooltipHint.tsx
@@ -4,7 +4,7 @@ import { browserIsMobile } from "../utilities/mobile";
 
 function KanaAnswerTooltipHint() {
   return (
-    <Text c="dimmed" fz="xs" opacity={0.5}>
+    <Text c="dimmed" fz="xs" opacity={0.7}>
       {browserIsMobile ? "Toque" : "Passe o mouse sobre"} o kana para revelar a resposta
     </Text>
   );
