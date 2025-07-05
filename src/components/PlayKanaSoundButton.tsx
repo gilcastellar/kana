0a443454190a4be71c538bc6889a3e91f28dac20
gilcastellar <gilcastellar@gmail.com>
2025-07-05 10:53:46 -0300

    Update PlayKanaSoundButton.tsx

--- src/components/PlayKanaSoundButton.tsx
+++ src/components/PlayKanaSoundButton.tsx
@@ -33,7 +33,7 @@ function PlayKanaSoundButton({ romaji }: PlayKanaSoundButtonProps) {
         if (audio === null) setAudio(new Audio(kanaAudioSource.djtguide(effectiveRomaji)));
       }}
     >
-      Play Sound
+      Tocar som
     </Button>
   );
 }
